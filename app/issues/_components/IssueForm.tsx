f7fb292ec6a95de278f897970d0a206141622bdb
Ayman Jaaouani <ayman.jaaouani.aj@gmail.com>
2023-10-22 23:37:56 +0300

    Dynamic skeleton issue form

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -7,16 +7,11 @@ import { Issue } from '@prisma/client';
 import { Button, Callout, TextField } from '@radix-ui/themes';
 import axios from 'axios';
 import 'easymde/dist/easymde.min.css';
-import dynamic from 'next/dynamic';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import { Controller, useForm } from 'react-hook-form';
+import SimpleMDE from 'react-simplemde-editor';
 import { z } from 'zod';
-
-const SimpleMDE = dynamic(() => import('react-simplemde-editor'), {
-	ssr: false,
-});
-
 interface Props {
 	issue?: Issue;
 }
@@ -39,8 +34,8 @@ const IssueForm = ({ issue }: Props) => {
 			setSubmitting(true);
 			if (issue) await axios.patch('/api/issues/' + issue.id, data);
 			else await axios.post('/api/issues', data);
-
 			router.push('/issues');
+			router.refresh();
 		} catch (error) {
 			setSubmitting(false);
 			setError('Unexpected error occured. Please try again');
